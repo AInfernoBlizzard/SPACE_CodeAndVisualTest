8fb28121d987b17a81c4e404f7e670b789a82cb0
AInfernoBlizzard <AS3661@live.mdx.ac.uk>
2022-11-14 13:20:28 +0000

    SPACE V1.5

--- New Unity Project (2)/Assets/Scripts/GameManager.cs
+++ New Unity Project (2)/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,6 +15,15 @@ public class GameManager : MonoBehaviour
     public int Equip;
     public int Quality;
     public int Popularity;
+
+
+    public int MoneyMultiplier = 1;
+    public int StaffMultiplier = 1;
+    public int EquipMultiplier = 1;
+    public int QualityMultiplier = 1;
+    public int PopularityMultiplier = 1;
+
+    public int PowerUpTicker;
 
     public Image MoneyBar;
     public Image StaffBar;
@@ -34,24 +44,34 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        generateEventDeck();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
+        
         MoneyBar.fillAmount = Money / 100.00f;
         StaffBar.fillAmount = Staff / 100.00f;
         EquipBar.fillAmount = Equip / 100f;
         QualityBar.fillAmount = Quality / 100f;
         PopularityBar.fillAmount = Popularity / 100f;
-        */
+        
         if (!eventSet)
         {
             DrawNewEvent();
             eventSet = true;
+            if (PowerUpTicker == 1)
+            {
+                ResetMultipliers();
+                PowerUpTicker = 0;
+            }
+            else
+            {
+                PowerUpTicker--;
+            }
         }
+
         
     }
 
@@ -87,11 +107,11 @@ public class GameManager : MonoBehaviour
         if(choice.text == eventDeck[0].TextButton1)
         {
             //update resources based on event choice
-            Money += eventDeck[0].Money_1;
-            Staff += eventDeck[0].Staff_1;
-            Equip += eventDeck[0].Equip_1;
-            Quality += eventDeck[0].Quality_1;
-            Popularity += eventDeck[0].Popularity_1;
+            Money += eventDeck[0].Money_1 * MoneyMultiplier;
+            Staff += eventDeck[0].Staff_1 * StaffMultiplier;
+            Equip += eventDeck[0].Equip_1 * EquipMultiplier;
+            Quality += eventDeck[0].Quality_1 * QualityMultiplier;
+            Popularity += eventDeck[0].Popularity_1 * PopularityMultiplier;
             //change to outcome text and button
             MainBodyText.text = eventDeck[0].Explanation_1;
             //disable one button
@@ -104,11 +124,11 @@ public class GameManager : MonoBehaviour
         if (choice.text == eventDeck[0].TextButton2)
         {
             //update resources based on event choice
-            Money += eventDeck[0].Money_2;
-            Staff += eventDeck[0].Staff_2;
-            Equip += eventDeck[0].Equip_2;
-            Quality += eventDeck[0].Quality_2;
-            Popularity += eventDeck[0].Popularity_2;
+            Money += eventDeck[0].Money_2 * MoneyMultiplier;
+            Staff += eventDeck[0].Staff_2 * StaffMultiplier;
+            Equip += eventDeck[0].Equip_2 * EquipMultiplier;
+            Quality += eventDeck[0].Quality_2 * QualityMultiplier;
+            Popularity += eventDeck[0].Popularity_2 * PopularityMultiplier;
             //change to outcome text and button
             MainBodyText.text = eventDeck[0].Explanation_2;
             //disable one button
@@ -118,6 +138,67 @@ public class GameManager : MonoBehaviour
             EndGameCheck();
         }
 
+        if (Staff < 1)
+        {
+            SceneManager.LoadScene("Staff0");
+        }
+
+        if (Staff > 99) 
+        {
+            SceneManager.LoadScene("Staff100");
+        }
+
+        if (Money < 1) 
+        {
+            SceneManager.LoadScene("Money0");
+        }
+
+    }
+
+
+    void ResetMultipliers()
+    {
+        //Deactivate the graphics
+        MoneyMultiplier = 1;
+        StaffMultiplier = 1;
+        EquipMultiplier = 1;
+        QualityMultiplier = 1;
+        PopularityMultiplier = 1;
+    }
+    void MoneyPowerUp(int NumberOfTurns)
+    {
+        ResetMultipliers();
+        MoneyMultiplier = 2;
+        PowerUpTicker = NumberOfTurns;
+        //Activate the graphic
+    }
+    void PopularityPowerUp(int NumberOfTurns)
+    {
+        ResetMultipliers();
+        PopularityMultiplier = 2;
+        PowerUpTicker = NumberOfTurns;
+        //Activate the graphic
+    }
+    void EquipmentPowerUp(int NumberOfTurns)
+    {
+        ResetMultipliers();
+        EquipMultiplier = 2;
+        PowerUpTicker = NumberOfTurns;
+        //Activate the graphic
+    }
+    void StaffPowerUp(int NumberOfTurns)
+    {
+        ResetMultipliers();
+        StaffMultiplier = 2;
+        PowerUpTicker = NumberOfTurns;
+        //Activate the graphic
+    }
+    void QualityPowerUp(int NumberOfTurns)
+    {
+        ResetMultipliers();
+        QualityMultiplier = 2;
+        PowerUpTicker = NumberOfTurns;
+        //Activate the graphic
     }
 
     public void EndGameCheck()
@@ -133,7 +214,6 @@ public class GameManager : MonoBehaviour
 
     public void EndGameExplanation()
     {
-
         ChoiceOneText.text = "Restart";
     }
 
@@ -145,6 +225,13 @@ public class GameManager : MonoBehaviour
 
     public void generateEventDeck()
     {
-
+        
+        for (int i = 0; i < eventDeck.Count; i++)
+        {
+            Event_Template temp = eventDeck[i];
+            int randomIndex = Random.Range(i, eventDeck.Count);
+            eventDeck[i] = eventDeck[randomIndex];
+            eventDeck[randomIndex] = temp;
+        }
     }
 }
